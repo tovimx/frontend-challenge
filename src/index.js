caebb10519601d170a72fa197deb799d4e0dce36
Tovi <tovimx@gmail.com>
2022-04-26 22:07:11 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom";
+import React from "react";
+import "./styles.scss";
+const App = () => {
+  return <h1>Frontend-challenge!</h1>;
+};
+ReactDOM.render(<App />, document.getElementById("app"));
